6d55b14cab2a3ae6ae3e433e41a9bd7d2e4a389b
Artur Judycki <artur.judycki55@gmail.com>
2022-09-24 22:37:58 +0200

    finish slider hero

--- js/hero.js
+++ js/hero.js
@@ -39,13 +39,87 @@ const showSlide = () => {
     index++;
   }
 
+  banerPhoto.style.backgroundImage = "url(" + tableOfImages[index] + ")";
+  banerText.textContent = tableOfTexts[index];
+  dots.forEach((dot) => dot.classList.remove("activeDot"));
+  dots[index].classList.add("activeDot");
+};
+
+let indexInterval = setInterval(showSlide, 5000);
+
+leftAngle.addEventListener("click", () => {
+  clearInterval(indexInterval);
+
+  if (index === 0) {
+    index = tableOfImages.length - 1;
+  } else {
+    index--;
+  }
+  banerPhoto.style.backgroundImage = "url(" + tableOfImages[index] + ")";
+  banerText.textContent = tableOfTexts[index];
+
   dots.forEach((dot) => dot.classList.remove("activeDot"));
   dots[index].classList.add("activeDot");
 
+  indexInterval = setInterval(showSlide, 5000);
+});
+
+rightAngle.addEventListener("click", () => {
+  clearInterval(indexInterval);
+  if (index === tableOfImages.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
   banerPhoto.style.backgroundImage = "url(" + tableOfImages[index] + ")";
-  // banerPhoto.style.backgroundImage = tableOfImages[index];
-
   banerText.textContent = tableOfTexts[index];
-};
 
-setInterval(showSlide, 5000);
+  dots.forEach((dot) => dot.classList.remove("activeDot"));
+  dots[index].classList.add("activeDot");
+
+  indexInterval = setInterval(showSlide, 5000);
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode === 37 || e.keyCode === 39) {
+    clearInterval(indexInterval);
+
+    if (e.keyCode === 37) {
+      if (index === 0) {
+        index = tableOfImages.length - 1;
+      } else {
+        index--;
+      }
+    } else {
+      if (index === tableOfImages.length - 1) {
+        index = 0;
+      } else {
+        index++;
+      }
+    }
+
+    banerPhoto.style.backgroundImage = "url(" + tableOfImages[index] + ")";
+    banerText.textContent = tableOfTexts[index];
+
+    dots.forEach((dot) => dot.classList.remove("activeDot"));
+    dots[index].classList.add("activeDot");
+
+    indexInterval = setInterval(showSlide, 5000);
+  }
+});
+
+dots.forEach((dot, indexDot) =>
+  dot.addEventListener("click", function () {
+    clearInterval(indexInterval);
+
+    index = indexDot;
+
+    banerPhoto.style.backgroundImage = "url(" + tableOfImages[index] + ")";
+    banerText.textContent = tableOfTexts[index];
+
+    dots.forEach((dot) => dot.classList.remove("activeDot"));
+    dots[index].classList.add("activeDot");
+
+    indexInterval = setInterval(showSlide, 5000);
+  })
+);
